29d97264ec8e105813189a87d5e9553ab46dc72c
Thanh Tùng Nguyễn <20222052@eaut.edu.vn>
2025-02-10 12:36:49 +0700

    commit all 7.1 8

--- taofile.py
+++ taofile.py
@@ -1,12 +1,16 @@
 import os
 
 # Thư mục để tạo các file
-directory = "BTTL\Code\TH06BT_TungNT"
+directory = "BTTL\Code\TH08VD_TungNT"
 
 # Danh sách tên file
 file_names = [
-    "bt4_1.py", "bt5_1.py", "bt5_2.py",
-    "bt6_1.py", "bt4_2.py"
+    "vd1.py", "vd2.py", "vd3.py",
+    "vd4.py", "vd5.py", "vd6.py",
+    "vd7.py", "vd8.py", "vd9.py",
+    "vd10.py", "vd11.py", "vd12.py",
+    "vd15.py", "vd14.py", "vd13.py",
+    "vd16.py"
     ]
 
 # Tạo thư mục nếu chưa tồn tại
